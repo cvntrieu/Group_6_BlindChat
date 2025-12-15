fd2b8e401ea0255c44fabef4013c79191746a598
Trieucvn <cvntrieu@gmail.comd>
2025-12-15 12:30:32 +0700

    Modify logo brand display

--- frontend/app/(app)/layout.tsx
+++ frontend/app/(app)/layout.tsx
@@ -12,21 +12,48 @@ export default async function Layout({ children }: LayoutProps) {
   return (
     <>
       <header className="fixed top-0 left-0 z-50 hidden w-full flex-row justify-between p-6 md:flex">
+
+        {/*
         <a
           target="_blank"
           rel="noopener noreferrer"
           href=""
           className="scale-100 transition-transform duration-300 hover:scale-110"
         >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
+          {/* eslint-disable-next-line @next/next/no-img-element 
           <img src={logo} alt={`${companyName} Logo`} className="block size-6 dark:hidden" />
-          {/* eslint-disable-next-line @next/next/no-img-element */}
+          {/* eslint-disable-next-line @next/next/no-img-element 
           <img
             src={logoDark ?? logo}
             alt={`${companyName} Logo`}
             className="hidden size-6 dark:block"
           />
         </a>
+        */}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="
+              scale-600
+              translate-x-9
+              translate-y-9
+              transform
+              transition-transform
+              duration-300
+            "
+          >
+            <img
+              src={logo}
+              alt={`${companyName} Logo`}
+              className="block size-6 dark:hidden"
+            />
+            <img
+              src={logoDark ?? logo}
+              alt={`${companyName} Logo`}
+              className="hidden size-6 dark:block"
+            />
+          </a>
+
         {/* <span className="text-foreground font-mono text-xs font-bold tracking-wider uppercase">
           Built with{' '}
           <a
